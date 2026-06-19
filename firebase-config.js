944dad5775e1e76b726d8de063e35050f7b28328
Karen Zhou <karen.zhou50@gmail.com>
2026-06-19 14:27:41 -0600

    api key no

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBxzhN0DuZR2xjLTA_nyGzCogTi5OibhLM",
+  apiKey: "AIzaSyAW-qBjFeG9OuAQ327UAZ0fGk2gqZSiIjA",
   authDomain: "polyquarket.firebaseapp.com",
   databaseURL: "https://polyquarket-default-rtdb.firebaseio.com",
   projectId: "polyquarket",
   storageBucket: "polyquarket.firebasestorage.app",
   messagingSenderId: "878294730665",
-  appId: "1:878294730665:web:cac1d6815c823bb06566b8"
+  appId: "1:878294730665:web:cac1d6815c823bb06566b8",
+  measurementId: "G-HLKSX7BRMB"
 };
