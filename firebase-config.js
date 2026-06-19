a466a11a6b661062d63f89b8e228e719fc9f49dd
Karen Zhou <karen.zhou50@gmail.com>
2026-06-19 14:28:32 -0600

    deleted API key

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,9 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAW-qBjFeG9OuAQ327UAZ0fGk2gqZSiIjA",
+  apiKey: "AIzaSyBxzhN0DuZR2xjLTA_nyGzCogTi5OibhLM",
   authDomain: "polyquarket.firebaseapp.com",
   databaseURL: "https://polyquarket-default-rtdb.firebaseio.com",
   projectId: "polyquarket",
   storageBucket: "polyquarket.firebasestorage.app",
   messagingSenderId: "878294730665",
-  appId: "1:878294730665:web:cac1d6815c823bb06566b8",
-  measurementId: "G-HLKSX7BRMB"
+  appId: "1:878294730665:web:cac1d6815c823bb06566b8"
 };
